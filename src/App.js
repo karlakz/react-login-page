f67f24cccb7ad75344219048042df094d696d7dc
Karlygash Yakiyayeva <53092602+karlakz@users.noreply.github.com>
2022-10-29 23:41:52 +0600

    [feat]: Add useEffect() function for storing user entered data and set user login info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserInfo === "True") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "True");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
   return (
